1c031a36fab1b67e0c03578894eee5ac6ab16c7f
reihan703 <reihanyasfinu@gmail.com>
2024-08-22 12:33:42 +0700

    WIP | change button to gpiozero

--- button_checker.py
+++ button_checker.py
@@ -1,13 +1,18 @@
 import RPi.GPIO as GPIO
-import time
+from gpiozero import DigitalInputDevice
 
 
 class ButtonChecker:
     def __init__(self, pin: int):
         self.pin = pin
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        # GPIO.setmode(GPIO.BCM)
+        # GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        self.button = DigitalInputDevice(pin=self.pin, pull_up=True)
 
+    # def is_pressed(self):
+    #     return GPIO.input(self.pin) == GPIO.HIGH
     def is_pressed(self):
-        return GPIO.input(self.pin) == GPIO.HIGH
+        if self.button.is_active:
+            return True
+        return False
 
